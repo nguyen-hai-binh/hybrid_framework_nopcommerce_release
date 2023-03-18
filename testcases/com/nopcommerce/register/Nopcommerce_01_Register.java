65baaaeee43a7e92a92f3bb7acd965215ed9ba0c
nguyen-hai-binh <haibinh.automationtester@gmail.com>
2023-03-18 20:25:14 +0700

    hybrid_framework_nopcommerce_release

---
+++ testcases/com/nopcommerce/register/Nopcommerce_01_Register.java
@@ -0,0 +1,46 @@
+package com.nopcommerce.register;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Nopcommerce_01_Register {
+	private WebDriver driver;
+
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+	}
+
+	@Test
+	public void Register_01_() {
+	}
+
+	@Test
+	public void Register_02_() {
+	}
+
+	@Test
+	public void Register_03_() {
+	}
+
+	@Test
+	public void Register_04_() {
+	}
+
+	@Test
+	public void Register_05_() {
+	}
+
+	@Test
+	public void Register_06_() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
