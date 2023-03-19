a068cd7bb3e4208d93d3bd5a80e29bfe7e116f7a
nguyen-hai-binh <haibinh.automationtester@gmail.com>
2023-03-19 22:13:57 +0700

    Update_BasePage

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,5 +1,46 @@
 package commons;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BasePage {
+	
+	private long longTimeout = 30;
+	//Web Brower Function
+	
+	protected void openPageURL(WebDriver driver, String pageUrl) {
+		driver.get(pageUrl);
+	}
+	
+	protected String getPageTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+	
+	protected String getPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+	
+	protected String getPageSourceCode(WebDriver driver) {
+		return driver.getPageSource();
+	}
+	
+	protected void backToPage(WebDriver driver) {
+		driver.navigate().back();
+	}
+	
+	protected void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+	
+	protected void refreshToPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+	
+	protected Alert waitForAlertPresence(WebDriver driver) {
+		WebDriverWait explicitWait = new WebDriverWait(driver, longTimeout);
+		return explicitWait.until(ExpectedConditions.alertIsPresent());
+	}
 
 }
